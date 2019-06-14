c6e97be9966cbcd24686645a6ae574e3b0130189
Ronak Patel <ronak1.patel@ryerson.ca>
2019-06-13 20:32:15 -0400

    added Font Awesome, added Navbar component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Navbar from "./components/layout/Navbar";
 import "./App.css";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <h1>Hello World!</h1>
       </div>
     );
