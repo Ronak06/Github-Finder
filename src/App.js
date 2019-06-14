c007e59d498ecd56482623b340b2444d31c1ab22
Ronak Patel <ronak1.patel@ryerson.ca>
2019-06-13 20:16:48 -0400

    changed App.js to class component, added App.css for styling.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "./App.css";
 
-const App = () => {
-  return <div>App</div>;
-};
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+}
 
 export default App;
