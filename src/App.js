388656d8d12882060fe5aa334a217e2a0c8f3bae
Ronak Patel <ronak1.patel@ryerson.ca>
2019-06-15 16:20:56 -0400

    added clear button to clear all the users in the users object, refactored code to make it more easier to read.

--- src/App.js
+++ src/App.js
@@ -48,13 +48,24 @@ class App extends React.Component {
     this.setState({ users: response.data.items, loading: false });
   };
 
+  // Clear Users from state
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
+
   render() {
+    const { users, loading } = this.state;
+
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search searchUsers={this.searchUsers} />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
