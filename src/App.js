5155f035a6c6bf5d7d7726c03c9f7e98c2857239
Ronak Patel <ronak1.patel@ryerson.ca>
2019-06-16 18:49:40 -0400

    added react-router-dom, added User file and About file which contains information about the specific user you select on the More button.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { Fragment } from "react";
 import axios from "axios";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 // import components and styling
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
+import About from "./components/Pages/About";
+import User from "./components/users/User";
 import "./App.css";
 
 class App extends React.Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   };
@@ -50,6 +54,24 @@ class App extends React.Component {
     this.setState({ users: response.data.items, loading: false });
   };
 
+  // Get single Github user
+  getUser = async username => {
+    // set loading to true, will cause app to rerender with Spinner
+    this.setState({ loading: true });
+
+    // Using axios to make get requests to Github api using Github token
+    // to allow for unlimited get requests
+    const response = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    // setting users array to contain all the data from the axios get request
+    // and setting loading to false now that all data has been extracted from axios
+    this.setState({ user: response.data, loading: false });
+  };
+
   // Clear Users from state
   clearUsers = () => {
     this.setState({ users: [], loading: false });
@@ -64,22 +86,47 @@ class App extends React.Component {
   };
 
   render() {
-    const { users, loading } = this.state;
+    const { user, users, loading } = this.state;
 
     return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Alert alert={this.state.alert} />
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users loading={loading} users={users} />
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={props => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" component={About} />
+              <Route
+                exact
+                path="/user/:login"
+                render={props => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    user={user}
+                    loading={loading}
+                  />
+                )}
+              />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </BrowserRouter>
     );
   }
 }
