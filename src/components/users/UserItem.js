7ff95bd0870125d0a571e4be347dc0f7ed720d3f
Ronak Patel <ronak1.patel@ryerson.ca>
2019-12-29 01:56:33 -0500

    converted UserItem into a Material-UI card.

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,51 +1,59 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles(theme => ({
-  bigAvatar: {
-    margin: 10,
-    width: 60,
-    height: 60,
-    float: "left"
-  },
   margin: {
     margin: theme.spacing(1)
   },
   extendedIcon: {
     marginRight: theme.spacing(1)
+  },
+  card: {
+    maxWidth: 345,
+    marginTop: 20
   }
 }));
 
 const UserItem = ({ user }) => {
   const classes = useStyles();
   return (
-    <div className="card text-center">
-      <Avatar
-        alt={user.login}
-        src={user.avatar_url}
-        className={classes.bigAvatar}
-      />
-
-      <h3>{user.login}</h3>
-      <p></p>
-      <div>
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt={user.login}
+          height="280"
+          image={user.avatar_url}
+          title={user.login}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {user.login}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          <a href={user.html_url} target="_blank" rel="noopener noreferrer">
+            GitHub
+          </a>
+        </Button>
         <Link to={`/user/${user.login}`}>
-          <Button
-            variant="contained"
-            size="small"
-            color="primary"
-            className={classes.margin}
-            to={`/user/${user.login}`}
-          >
+          <Button size="small" color="primary">
             Learn More
           </Button>
         </Link>
-      </div>
-    </div>
+      </CardActions>
+    </Card>
   );
 };
 
